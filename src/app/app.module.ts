541680a74a996f723e64f8bd54c55ae4afb67b38
unknown <Ivan A>
2018-02-28 20:41:18 +0300

    Добавил меню, которое переключает компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { HomeComponent } from './home/home.component';
 import { EventsComponent } from './events/events.component';
 
 import { RouterModule, Routes } from '@angular/router';
+import { MembershipComponent } from './membership/membership.component';
+import { TrainersComponent } from './trainers/trainers.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'events', component: EventsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'membership', component: MembershipComponent },
+  { path: 'trainers', component: TrainersComponent },
+  { path: 'contacts', component: ContactsComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EventsComponent
+    EventsComponent,
+    MembershipComponent,
+    TrainersComponent,
+    ContactsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
